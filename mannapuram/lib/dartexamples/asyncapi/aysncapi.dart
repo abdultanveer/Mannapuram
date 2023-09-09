3963e0894371f29bd38383cb3d8787b64e962bae
Abdul <abdul.tanveer@gmail.com>
2023-09-09 11:36:36 +0530

    added http dependency

--- mannapuram/lib/dartexamples/asyncapi/aysncapi.dart
+++ mannapuram/lib/dartexamples/asyncapi/aysncapi.dart
@@ -1,16 +1,17 @@
-void main()async{
+void main() async {
   print(await createOrderCoffee());
 }
 
 Future<String> createOrderCoffee() async {
-   var coffee =  await prepareCoffee();
+  print("instruction no 1");
+  var coffee = await prepareCoffee();
+  print("instruction no 2");
 
   return 'your order is $coffee';
 }
 
 Future<String> prepareCoffee() {
-   return Future.delayed(Duration(seconds: 2),
-      (){
-     return   'large latte';
-      });
+  return Future.delayed(Duration(seconds: 2), () {
+    return 'large latte';
+  });
 }
